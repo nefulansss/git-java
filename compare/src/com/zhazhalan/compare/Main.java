271c3a7fc3d5f4359ae3c2c7dc7a52c3aaa7d2e7
zhanglan61 <zhanglan61@jd.com>
2020-05-24 10:02:02 +0800

    练习1

--- compare/src/com/zhazhalan/compare/Main.java
+++ compare/src/com/zhazhalan/compare/Main.java
@@ -1,8 +1,9 @@
-package com.zhazhalan;
+package com.zhazhalan.compare;
 
 import com.zhazhalan.compare.Dog;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -15,11 +16,15 @@ public class Main {
         List<Dog> list = new ArrayList<>(30);
         for (int i=0;i<10;i++){
             Dog dog =new Dog("狗子"+i,i);
+            Dog dog1 =new Dog("狗子"+(10-i),(10-i));
             list.add(dog);
-            if (i==9){
-                i=0;
-            }
+            list.add(dog1);
         }
+        System.out.println("输出狗子");
         list.stream().forEach(System.out::println);
+        Collections.sort(list);
+        System.out.println("排序后");
+        list.stream().forEach(System.out::println);
+
     }
 }
---
+++ compare/src/com/zhazhalan/thread/MyThread.java
@@ -0,0 +1,15 @@
+package com.zhazhalan.thread;
+
+/**
+ * @Author: zhanglan61
+ * @Date: 2020/5/24 9:58
+ * @Version: 1.0
+ */
+public class MyThread extends Thread {
+
+
+    @Override
+    public void run() {
+        System.out.println("我是mythread线程");
+    }
+}
